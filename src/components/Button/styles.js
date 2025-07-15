bf43f203bf61ec8726605ece2f2f49f8a4923aa9
wellinbraga <wellinbraga@gmail.com>
2025-07-15 20:59:28 -0300

    first program react

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+        padding: 10px;
+        boder: 1px solid #cdcdcd;
+        background-color: #00aaf0;
+        color: #ffffff;
+        font-size: 24px;
+        font-weight: 700;
+        flex: 1;
+
+        &:hover{
+                opacity: 0.6;        
+        }
+
+`
